83cab618cbf68dc03418947afc6d410b5e99b89f
yurus0 <imanebenrazzouk0302@gmail.com>
2023-12-17 08:47:14 +0100

    feat: Glue applied:AccountSteps glued with gherkin scenario;

--- bank/src/test/java/com/bank/AccountSteps.java
+++ bank/src/test/java/com/bank/AccountSteps.java
@@ -3,19 +3,23 @@ package com.bank;
 public class AccountSteps {
     private Account account;
     
+    @Given("a new account with balance {int}")
     public void createAccount(int initBalance){
         account = new Account();
         account.deposit(initBalance);
     }
 
+    @When("I deposit {int} from the account")
     public void withdrawFromAccount(int amount){
         account.withdraw(amount);
     }
 
+    @When("I withdraw {int} from the account")
     public void depositIntoAccount(int amount){
         account.deposit(amount);
     }
 
+    @Then("the balance should be {int}")
     public void verifyBalance(int expectedBalance){
         assertEquals(expectedBalance, account.getBalance());
     }
